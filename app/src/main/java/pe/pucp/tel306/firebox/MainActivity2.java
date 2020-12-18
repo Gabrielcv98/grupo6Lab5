15d9bf32aa848e8f7855f95cdc5805949f386696
eric.benites <eric.benites@pucp.edu.pe>
2020-12-18 11:27:35 -0500

    button +

--- app/src/main/java/pe/pucp/tel306/firebox/MainActivity2.java
+++ app/src/main/java/pe/pucp/tel306/firebox/MainActivity2.java
@@ -1,19 +1,28 @@
 package pe.pucp.tel306.firebox;
 
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Context;
+import android.content.Intent;
 import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
+import android.widget.Toast;
 
+import com.google.android.gms.dynamic.IFragmentWrapper;
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 
 public class MainActivity2 extends AppCompatActivity {
+
+    FloatingActionButton agregar, foto, doc;
+    Boolean isAllFabVisible;
+    private final int CHOOSE_PDF_FROM_DEVICE = 1001;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -57,6 +66,67 @@ public class MainActivity2 extends AppCompatActivity {
                 onBackPressed();
             }
         });
+
+        agregar= findViewById(R.id.add_fab);
+        foto = findViewById(R.id.add_foto_fab);
+        doc = findViewById(R.id.add_doc_fab);
+
+        foto.setVisibility(View.GONE);
+        doc.setVisibility(View.GONE);
+
+        isAllFabVisible = false;
+
+        agregar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (!isAllFabVisible){
+                    foto.show();
+                    doc.show();
+                    isAllFabVisible = true;
+                }else {
+                    foto.hide();
+                    doc.hide();
+                    isAllFabVisible = false;
+                }
+            }
+        });
+
+        doc.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(MainActivity2.this, "subee", Toast.LENGTH_SHORT).show();
+
+                Log.d("infoApp", "aquí");
+                obtenerPath();
+            }
+        });
+
+
+
+
+    }
+
+    public void obtenerPath (){
+        Intent intent = new Intent(Intent.ACTION_OPEN_DOCUMENT);
+        intent.addCategory(Intent.CATEGORY_OPENABLE);
+        intent.setType("application/pdf");
+        Log.d("infoApp", "llegaste");
+        startActivityForResult(intent, CHOOSE_PDF_FROM_DEVICE);
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (requestCode == CHOOSE_PDF_FROM_DEVICE && resultCode == RESULT_OK){
+            if (data != null){
+                Log.d("infoApp", data.getData().toString());
+
+            }
+        }
+    }
+
+    public  void subirArchivo (View view){
+        Toast.makeText(this, "holoaaaa", Toast.LENGTH_SHORT).show();
 
     }
 }
